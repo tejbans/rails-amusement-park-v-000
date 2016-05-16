74d9c0295c0f690fe07076f68b40133757d66cba
tejbans <singh.tejbans@gmail.com>
2016-05-16 11:25:17 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,26 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to  :user
+  belongs_to  :attraction
+
+  def take_ride
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      start_ride
+
+    end
+  end
+
+  def start_ride
+    new_tickets = user.tickets - attraction.tickets
+    new_nausea = user.nausea + attraction.nausea_rating
+    new_happiness = user.happiness + attraction.happiness_rating
+    user.update(:tickets => new_tickets, :nausea => new_nausea, :happiness => new_happiness)
+     "Thanks for riding the #{self.attraction.name}!"
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many  :attractions, through: :rides
+
+  def mood
+    if happiness && nausea
+      nausea > happiness ? "sad" : "happy"
+    end
+  end
+
 end
